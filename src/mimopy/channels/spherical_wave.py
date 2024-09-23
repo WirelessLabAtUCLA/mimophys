fixed spherical wave normalization

src/mimopy/channels/spherical_wave.py
@@ -27,7 +27,7 @@ class SphericalWaveChannel(Channel):
         d = np.sqrt(dx**2 + dy**2 + dz**2)
         # get relative phase shift
         phase_shift = 2 * np.pi * d
-        self.channel_matrix = np.exp(1j * phase_shift).T.conj()
+        self.channel_matrix = 1 / d * np.exp(1j * phase_shift).T.conj()
         self.normalize_energy(self._energy)
         return self
     