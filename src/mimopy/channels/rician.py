415a1d7d2a0cbd94b2cc4839e5c445099aa7664c
Samuel <samuel.li@ucla.edu>
2024-09-23 14:48:07 -0700

    added rician return options

--- src/mimopy/channels/rician.py
+++ src/mimopy/channels/rician.py
@@ -37,13 +37,14 @@ class RicianChannel(Channel):
             self.los = LoSChannel(tx, rx, path_loss, seed=self.seed)
         self.nlos = RayleighChannel(tx, rx, path_loss, seed=self.seed)
 
-    def generate_channels(self, n_channels=1):
+    def generate_channels(self, n_channels=1, return_subchannels=False):
         """Generate multiple channel matrices with static LoS component."""
         H_los = self.los.realize().H
         H_nlos = self.nlos.generate_channels(n_channels)
-        return (
-            np.sqrt(self.K / (self.K + 1)) * H_los + np.sqrt(1 / (self.K + 1)) * H_nlos
-        )
+        H = np.sqrt(self.K / (self.K + 1)) * H_los + np.sqrt(1 / (self.K + 1)) * H_nlos
+        if return_subchannels:
+            return H, H_los, H_nlos
+        return H
 
     def realize(self):
         """Realize the channel."""
