many bug fixes for aoa and aod calculation: LoS and SphericalWave should match now

src/mimophys/channels/awgn.py
@@ -6,6 +6,7 @@ import numpy.linalg as LA
 from numpy import log2, log10
 
 from ..devices.antenna_array import AntennaArray
+from ..utils.geometry import relative_position
 from .path_loss import PathLoss, get_path_loss
 
 
@@ -129,6 +130,17 @@ class Channel:
     # ========================================================
     # Measurements
     # ========================================================
+
+    @property
+    def aoa(self):
+        """Line-of-sight angle of arrival (AoA) in radians."""
+        return relative_position(self.tx.array_center, self.rx.array_center)[1:]
+
+    @property
+    def aod(self):
+        """Line-of-sight angle of departure (AoD) in radians."""
+        return relative_position(self.rx.array_center, self.tx.array_center)[1:]
+
     @property
     def rx_power(self):
         """Received power in linear scale."""
@@ -150,8 +162,8 @@ class Channel:
     def bf_gain(self) -> float:
         """Normalized beamforming gain |wHf|^2 / Nt in linear scale."""
         f = self.tx.weights.reshape(-1, 1)
-        w = self.rx.weights.reshape(-1, 1)
-        return float(np.abs(w.T @ self.H @ f) ** 2 / (self.tx.N * LA.norm(w) ** 2))
+        w = self.rx.weights.reshape(1, -1)
+        return float(np.abs(w @ self.H @ f) ** 2 / (self.tx.N * LA.norm(w) ** 2))
 
     @property
     def bf_gain_db(self) -> float:

src/mimophys/channels/los.py
@@ -31,20 +31,17 @@ class LoSChannel(Channel):
         _, az, el = relative_position(self.tx.array_center, self.rx.array_center)
         return az, el
 
-    @aoa.setter
-    def aoa(self, _):
-        raise Warning("Use realize() to set the AoA/AoD, ignoring the input.")
 
-    aod = aoa
-
-    def generate_channels(self, az: np.ndarray, el: np.ndarray, grid=False) -> np.ndarray:
+    def generate_channels(
+        self, az: np.ndarray, el: np.ndarray, grid=False
+    ) -> np.ndarray:
         """Batch generate channel matrices for given AoA/AoD.
         :param az: Azimuth angles in radians.
         :param el: Elevation angles in radians.
         :param grid: If True, generate channel matrix for all combinations of az and el.
                     If False, generate channel matrix for each pair of az and el.
         """
-        
+
         tx_response = self.tx.get_array_response(az, el, grid=False)
         rx_response = self.rx.get_array_response(az + np.pi, el + np.pi, grid=grid)
         if len(tx_response.shape) == 1:
@@ -59,7 +56,7 @@ class LoSChannel(Channel):
         # TODO: warning if channel matrix not updated/realized
         _, az, el = relative_position(self.tx.array_center, self.rx.array_center)
         tx_response = self.tx.get_array_response(az, el)
-        rx_response = self.rx.get_array_response(az + np.pi, el + np.pi)
+        rx_response = self.rx.get_array_response(az, el)
         self.H = np.outer(rx_response, tx_response.conj())
         self.normalize_energy(self.channel_energy)
         return self

src/mimophys/devices/antenna_array.py
@@ -458,55 +458,11 @@ class AntennaArray:
         # translate the array back to its original position
         self.array_center = array_center
 
-    # def _rotate(self, coordinates, x_angle, y_angle, z_angle):
-    #     """Rotate the array by the given angles.
-
-    #     Args:
-    #         coordinates: Coordinates to rotate.
-    #         x_angle: Angle of rotation about the x-axis in radians.
-    #         y_angle: Angle of rotation about the y-axis in radians.
-    #         z_angle: Angle of rotation about the z-axis in radians.
-
-    #     Returns:
-    #         numpy.ndarray: Rotated coordinates.
-    #     """
-    #     rotation_matrix = np.array(
-    #         [
-    #             [
-    #                 np.cos(y_angle) * np.cos(z_angle),
-    #                 np.cos(z_angle) * np.sin(x_angle) * np.sin(y_angle)
-    #                 - np.cos(x_angle) * np.sin(z_angle),
-    #                 np.cos(x_angle) * np.cos(z_angle) * np.sin(y_angle)
-    #                 + np.sin(x_angle) * np.sin(z_angle),
-    #             ],
-    #             [
-    #                 np.cos(y_angle) * np.sin(z_angle),
-    #                 np.cos(x_angle) * np.cos(z_angle)
-    #                 + np.sin(x_angle) * np.sin(y_angle) * np.sin(z_angle),
-    #                 -np.cos(z_angle) * np.sin(x_angle)
-    #                 + np.cos(x_angle) * np.sin(y_angle) * np.sin(z_angle),
-    #             ],
-    #             [
-    #                 -np.sin(y_angle),
-    #                 np.cos(y_angle) * np.sin(x_angle),
-    #                 np.cos(x_angle) * np.cos(y_angle),
-    #             ],
-    #         ]
-    #     )
-
-    #     translate_coordinates = self.translate()  # center the array at the origin
-    #     self.coordinates = np.dot(coordinates, rotation_matrix)  # rotate the array
-    #     self.translate(
-    #         -translate_coordinates
-    #     )  # translate the array back to its original position
-
-    #     return np.dot(coordinates, rotation_matrix)
-
     ############################
     # Get AntennaArray Properties
     ############################
 
-    def get_array_response(self, az=0, el=0, grid=True, use_degrees=False):
+    def get_array_response(self, az=0, el=0, use_degrees=False):
         """Returns the array response vector at a given azimuth and elevation.
 
         This response is simply the phase shifts experienced by the elements
@@ -527,18 +483,24 @@ class AntennaArray:
             (len(az), len(el), len(coordinates)) and is squeezed if az and/or el are scalars.
         """
         # calculate the distance of each element from the first element
-        dx = self.coordinates[:, 0] - self.coordinates[0, 0]
-        dy = self.coordinates[:, 1] - self.coordinates[0, 1]
-        dz = self.coordinates[:, 2] - self.coordinates[0, 2]
+        # dx = self.coordinates[:, 0] - self.coordinates[0, 0]
+        # dy = self.coordinates[:, 1] - self.coordinates[0, 1]
+        # dz = self.coordinates[:, 2] - self.coordinates[0, 2]
 
-        dx = dx[np.newaxis, np.newaxis, :]
-        dy = dy[np.newaxis, np.newaxis, :]
-        dz = dz[np.newaxis, np.newaxis, :]
+        # dx = dx[np.newaxis, np.newaxis, :]
+        # dy = dy[np.newaxis, np.newaxis, :]
+        # dz = dz[np.newaxis, np.newaxis, :]
 
-        el_shape = (1, -1, 1) if grid else (-1, 1, 1)
-        el = np.array(el).reshape(*el_shape)
-        az = np.array(az).reshape(-1, 1, 1)
+        # el_shape = (-1, 1, 1)
+        # el = np.array(el).reshape(*el_shape)
+        # az = np.array(az).reshape(-1, 1, 1)
 
+        dx = (self.coord_x - self.coord_x[0]).reshape(1, -1)
+        dy = (self.coord_y - self.coord_y[0]).reshape(1, -1)
+        dz = (self.coord_z - self.coord_z[0]).reshape(1, -1)
+
+        az = np.asarray(az).reshape(-1, 1)
+        el = np.asarray(el).reshape(-1, 1)
         if use_degrees:
             az = np.deg2rad(az)
             el = np.deg2rad(el)
@@ -551,9 +513,10 @@ class AntennaArray:
                 + dz * np.sin(el)
             )
         )
-        array_response = np.squeeze(array_response)
-        if self.num_antennas == 1:
-            array_response = array_response.reshape(-1, 1)
+        # array_response = np.squeeze(array_response, axis=0)
+        # if self.num_antennas == 1:
+        #     array_response = array_response.reshape(-1, 1)
+
         return array_response
 
     def get_array_gain(self, az, el, db=True, use_degrees=True):
@@ -586,38 +549,6 @@ class AntennaArray:
 
     get_gain = get_array_gain
 
-    def conjugate_beamformer(self, az=0, el=0):
-        """Returns the conjugate beamformer at a given azimuth and elevation.
-
-        Args:
-            az: Azimuth angle in degrees.
-            el: Elevation angle in degrees.
-
-        Returns:
-            numpy.ndarray: Conjugate beamformer weights.
-        """
-        array_response_vector = self.get_array_response(
-            az * np.pi / 180, el * np.pi / 180
-        )
-        return array_response_vector
-
-    def get_array_pattern_azimuth(self, el, num_points=360, range=360):
-        """Returns the array pattern at a given elevation.
-
-        Args:
-            el: Elevation angle in radians.
-            num_points: Number of points at which the pattern is to be calculated.
-                Default is 360.
-            range: Range of azimuth angles in degrees. Default is 360.
-
-        Returns:
-            numpy.ndarray: Array pattern values at specified azimuth points.
-        """
-        az = np.linspace(-range / 2, range / 2, num_points) * np.pi / 180
-        return self.get_array_response(az, el)
-
-    array_pattern_azimuth = get_array_pattern_azimuth
-
     ############################
     # Plotting
     ############################
@@ -647,36 +578,10 @@ class AntennaArray:
         """
         return plot_arrays_3d(self, **kwargs)
 
-    def plot_gain_el(self, angle=0, angle_range=np.linspace(-89, 89, 178), **kwargs):
-        """Plot the array pattern along elevation at a given azimuth.
-
-        Args:
-            angle: Azimuth angle in degrees.
-            angle_range: Range of elevation angles to plot.
-            **kwargs: Additional arguments passed to plot_gain.
-
-        Returns:
-            matplotlib.axes.Axes: The axes object with the plot.
-        """
-        return self.plot_gain(angle=angle, angle_range=angle_range, axis="el", **kwargs)
-
-    def plot_gain_az(self, angle=0, angle_range=np.linspace(-89, 89, 178), **kwargs):
-        """Plot the array pattern along azimuth at a given elevation.
-
-        Args:
-            angle: Elevation angle in degrees.
-            angle_range: Range of azimuth angles to plot.
-            **kwargs: Additional arguments passed to plot_gain.
-
-        Returns:
-            matplotlib.axes.Axes: The axes object with the plot.
-        """
-        return self.plot_gain(angle=angle, angle_range=angle_range, axis="az", **kwargs)
-
     def plot_gain(
         self,
         weights: ArrayLike = None,
-        axis: str = "el",
+        axis: str = "az",
         angle: float = 0,
         angle_range: ArrayLike = np.linspace(-89, 89, 356),
         use_degrees: bool = True,
@@ -690,8 +595,8 @@ class AntennaArray:
         Args:
             weights (ArrayLike, optional): Weights of the antennas. If not given,
                 the weights of the array are used.
-            along (str): Axis along which the gain is to be plotted.
-                Takes value 'el'/'elevation' or 'az'/'azimuth'. Default is 'el'.
+            axis (str): Axis along which the gain is to be plotted.
+                Takes value 'el' (elevation) or 'az' (azimuth). Default is 'az'.
             angle (float): Angle at which the gain is to be plotted along the given axis.
             angle_range (ArrayLike): Range of angles at which the gain is to be plotted.
             use_degrees (bool): If True, the angles are in degrees. Default is True.
@@ -702,17 +607,26 @@ class AntennaArray:
         Returns:
             matplotlib.axes.Axes: The axes object with the plot.
         """
+
         if weights is not None:
             orig_weights = self.get_weights()
             self.set_weights(weights)
-        if axis == "el" or axis == "elevation":
-            el = np.asarray(angle) * np.pi / 180
-            az = np.asarray(angle_range) * np.pi / 180
-        elif axis == "az" or axis == "azimuth":
-            az = np.asarray(angle) * np.pi / 180
-            el = np.asarray(angle_range) * np.pi / 180
+
+        if not use_degrees:
+            angle = np.rad2deg(angle)
+            angle_range = np.rad2deg(angle_range)
+
+        if axis == "az":
+            el = np.asarray(angle)
+            az = np.asarray(angle_range)
+        elif axis == "el":
+            az = np.asarray(angle)
+            el = np.asarray(angle_range)
         else:
-            raise ValueError("`along` must be 'el'/'elevation or 'az'/'azimuth'.")
+            raise ValueError("axis must be 'az' or 'el'")
+
+        az = np.deg2rad(az)
+        el = np.deg2rad(el)
 
         # vectorized version
         gain = self.get_array_gain(az, el, db=db, use_degrees=False)
@@ -740,8 +654,8 @@ class AntennaArray:
             ax.set_xlabel("Azimuth (deg)" if axis == "el" else "Elevation (deg)")
             ax.set_ylabel("Gain (dB)")
 
-        axis_name = "el" if axis == "el" else "az"
-        title = f"{axis_name} = {angle} deg, max gain = {np.max(np.abs(gain)):.2f} dB"
+        title = f"{axis} = {angle} deg, max gain = {np.max(np.abs(gain)):.2f} dB"
+
         ax.set_title(title)
         ax.grid(True)
         if weights is not None:
@@ -749,6 +663,11 @@ class AntennaArray:
         if ax is None:
             plt.tight_layout()
             plt.show()
+
+        if axis == "el":
+            if polar:
+                ax.set_theta_zero_location("W")
+
         return ax
 
     def plot_gain_3d(
@@ -787,9 +706,10 @@ class AntennaArray:
         if use_degrees:
             az = np.deg2rad(az)
             el = np.deg2rad(el)
+        
+        AZ, EL = np.meshgrid(az, el)
 
         gain = self.get_array_gain(az, el, db=dB, use_degrees=False)
-        AZ, EL = np.meshgrid(az, el)
 
         if max_gain is None:
             max_gain = np.max(gain)

src/mimophys/utils/geometry.py
@@ -22,15 +22,10 @@ def relative_position(tx, rx) -> Tuple[float, float, float]:
     el: float
         Elevation angle.
     """
-    # if hasattr(tx, 'array_center'):
-    #     tx = tx.array_center
-    # if hasattr(rx, 'array_center'):
-    #     rx = rx.array_center
-
     tx = np.asarray(tx).reshape(3)
     rx = np.asarray(rx).reshape(3)
-    dxyz = dx, dy, dz = rx - tx
-    r = np.linalg.norm(dxyz)
+    dx, dy, dz = tx - rx
+    r = np.sqrt(dx**2 + dy**2 + dz**2)
     az = np.arctan2(dx, dy)
     el = np.arcsin(dz / r)
     return r, az, el